625ba1a2dd163ec91c9eb561b3eb8b05ddc1129f
Arthur Machado <arthur.machado@maxmilhas.com.br>
2023-02-02 09:43:15 -0300

    feat: post iteration

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,74 @@ import { Post } from "./components/Post";
 import styles from './App.module.css';
 import './global.css';
 
+// data from backend;
+const posts = [
+  {
+    id: 1,
+    content: [
+      {
+        id: 1,
+        type: 'paragraph',
+        content: 'Hello everybody 👋'
+      },
+      {
+        id: 2,
+        type: 'paragraph',
+        content: "I just uploaded another project to my portfolio. It's a project I did at NLW Return, a Rocketseat event. Project name is DoctorCare 🚀"
+      },
+      {
+        id: 3,
+        type: 'link',
+        content: 'naracarvalhoc.github.io/DoctorCare'
+      },
+    ],
+    published_at: new Date('2023-01-20T22:00:00'),
+    author: {
+      id: 1,
+      name: 'Nara Carvalho',
+      avatar_url: 'https://github.com/naracarvalhoc.png',
+      role: 'Programmer Student',
+    }
+  },
+  {
+    id: 2,
+    content: [
+      {
+        id: 4,
+        type: 'paragraph',
+        content: 'Hey, there! 👋'
+      },
+      {
+        id: 5,
+        type: 'paragraph',
+        content: "I would like to share with you this massive study project that I am developing using NodeJS with Nest, GraphQL and Postgres, in order to improve my skills with these technologies, in addition to using the concepts of SOLID, TDD and DDD."
+      },
+      {
+        id: 6,
+        type: 'paragraph',
+        content: "It's still under construction, but it's turning out pretty cool. Go check it out and leave that star!"
+      },
+      {
+        id: 7,
+        type: 'paragraph',
+        content: 'Follow the link'
+      },
+      {
+        id: 8,
+        type: 'link',
+        content: 'https://bruncarvalho.github.io/cracha-nlw/'
+      },
+    ],
+    published_at: new Date('2023-02-01T22:00:00'),
+    author: {
+      id: 2,
+      name: 'Bruna Carvalho',
+      avatar_url: 'https://github.com/bruncarvalho.png',
+      role: 'Intern Development',
+    }
+  }
+]
+
 export function App() {
 
   return (
@@ -18,10 +86,17 @@ export function App() {
         </aside>
 
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+              return (
+                <Post
+                  key={post.id}
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.published_at}
+                />
+              )
+          })}
         </main>
-
       </div>
 
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,33 +1,45 @@
+import { format, formatDistanceToNow} from 'date-fns';
+import ptBR from 'date-fns/esm/locale/pt-BR/index.js';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
+
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post({author, publishedAt, content}) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
           <Avatar
-            src="https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=360&q=80" 
+            src={author.avatar_url}
           />
           <div className={styles.authorInfo}>
-            <strong>Arthur Machado</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title='December 20, 2022 09:58 pm' dateTime='2022-12-20'>Posted 1 hour</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p>👉{' '}<a href=''>jane.design/doctorcare</a></p>
-        <p>
-          <a href=''>#novoprojeto</a>{' '}
-          <a href=''>#nlw</a>{' '}
-          <a href=''>#rocketseat</a>
-        </p>
+        {content.map(line => {
+          if (line.type == 'paragraph') {
+            return <p>{line.content}</p>;
+          } else if (line.type === 'link') {
+            return <p><a href="#">{line.content}</a></p>;
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
